35ad1afb49857859ea42c4e3dd9f007baa914706
ark930 <wangxiao_8800@qq.com>
2015-03-11 15:58:08 +0800

    add configs

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,13 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/itbook.db'
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['WTF_CSRF_ENABLED'] = True
 
 db = SQLAlchemy(app)
 db.create_all()
 
 from book.views import book_blueprint
 app.register_blueprint(book_blueprint)
-
-
-@app.route('/')
-def hello():
-    return 'hello world'

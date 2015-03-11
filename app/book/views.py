facf81f436f158c8f84f2d1a4ff7ee3b74105b60
ark930 <wangxiao_8800@qq.com>
2015-03-11 16:00:21 +0800

    update views.py

--- app/book/views.py
+++ app/book/views.py
@@ -3,15 +3,21 @@ from os import path
 
 from flask import Blueprint
 from flask import render_template
+from flask import redirect
 import requests
 
 from models import Book
 from pagination import Pagination
+from search_form import SearchForm
 
 
 book_blueprint = Blueprint('book', __name__)
 
 base_url = 'http://it-ebooks-api.info/v1'
+
+@book_blueprint.route('/')
+def index():
+    return 'hello'
 
 
 @book_blueprint.route('/book/<int:id>')
@@ -28,11 +34,6 @@ def get_book(id):
     return do_error(data['Error'])
 
 
-# @book_blueprint.route('/book', methods=['POST'])
-# def add_book():
-# return 'post book'
-
-
 @book_blueprint.route('/books/<string:keywords>')
 @book_blueprint.route('/books/<string:keywords>/page/<int:number>')
 def get_books(keywords, number=1):
@@ -44,19 +45,30 @@ def get_books(keywords, number=1):
         request_url = path.join(request_url, str(number))
 
     r = requests.get(request_url)
-
     data = r.json()
     if data['Error'] == '0':
-        books = []
-        for b in data['Books']:
-            book = Book(**b)
-            books.append(book)
+        if int(data['Total']) > 0:
+            books = []
+            for b in data['Books']:
+                book = Book(**b)
+                books.append(book)
 
-        pagination = Pagination((int(data['Page'])+10)/10, 10, int(data['Total']))
-
-        return render_template('books.html', books=books, pagination=pagination, keywords=keywords)
+            pagination = Pagination((int(data['Page'])+10)/10, 10, int(data['Total']))
+            return render_template('books.html', books=books, pagination=pagination, keywords=keywords)
+        else:
+            return 'No data'
 
     return do_error(data['Error'])
+
+
+@book_blueprint.route('/search', methods=['GET', 'POST'])
+def do_search():
+    form = SearchForm()
+    if form.validate_on_submit():
+        redirect_url = path.join('/books', form.data['keywords'])
+        return redirect(redirect_url)
+
+    return render_template('/home.html', form=form)
 
 
 def do_error(error_no):
